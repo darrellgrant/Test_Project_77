a237052e5ae17876dc2f72c0299cf9b9a10bc35d
Darrell Grant <dlsan3000@gmail.com>
2021-10-30 18:21:26 -0400

    Merge branch 'darrell_2909' into main

--- test.js
+++ test.js
@@ -1,3 +1,33 @@
-alert("Hello Planet Earth");
-alert("Hey");
-alert("Hey Sailor");
+//named function
+function showAlerts() {
+  alert("Hello Planet Earth");
+  alert("Hey");
+  alert("Hey Sailor");
+  alert("Wanna come up to my place and play Nintendo 64?");
+}
+showAlerts();
+
+//"assigned" function
+myAlerts = function () {
+  alert("Holey MOLEY");
+  alert("Yo Joe!");
+};
+
+myAlerts();
+
+
+//event handler
+function showNewAlerts() {
+  var myButton = document.getElementById("button");
+  myButton.onclick = function () {
+    alert("THIS IS ALL CAPS");
+  };
+}
+showNewAlerts();
+
+
+//event listener
+var myGreyButton = document.getElementById("newButton");
+myGreyButton.addEventListener("click", function () {
+  alert("THIS IS ALL ABOUT MY NEW BUTTON BABY");
+});
